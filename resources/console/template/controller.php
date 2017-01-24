e7e05fe3a597d21fba1c86a2abf4af8e592011a2
Pablo de la Peña <p@shedcollective.org>
2017-01-24 12:37:31 +0000

    Template for console app

---
+++ resources/console/template/controller.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is the template for the contents of API controllers
+ * Used by the console command when creating API controllers.
+ */
+
+return <<<'EOD'
+<?php
+
+/**
+ * The {{MODEL_NAME}} API controller
+ *
+ * @package  App
+ * @category controller
+ */
+
+namespace Nails\Api\App;
+
+use Nails\Api\Controller\DefaultController;
+
+class {{CLASS_NAME}} extends DefaultController
+{
+    const CONFIG_MODEL_NAME     = '{{MODEL_NAME}}';
+    const CONFIG_MODEL_PROVIDER = '{{MODEL_PROVIDER}}';
+}
+
+EOD;
