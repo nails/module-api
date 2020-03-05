b8087e5d9799e11024ca5c2b7781f76271072801
Pablo de la Peña <p@shedcollective.org>
2020-03-05 11:37:32 +0000

    Adds Api\Constants class

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\Api;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Api
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-api';
+}
